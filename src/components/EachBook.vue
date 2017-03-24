dadae5c510c9192e9e31bd13f4fabb1021e1d57b
thanawatumarsa <thanawatumarsa@gmail.com>
2017-03-24 23:25:56 +0700

    Test Javascript

--- src/components/EachBook.vue
+++ src/components/EachBook.vue
@@ -14,7 +14,28 @@
           <span class="glyphicon glyphicon-trash" v-on:click="removebook(book)"></span>
         </td>
       </tr>
+      <!-- <nav class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">WebsiteName</a>
+              </div>
+              <div class="collapse navbar-collapse" id="collapse">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Page 1</a></li>
+                  <li><a href="#">Page 2</a></li>
+                  <li><a href="#">Page 3</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav> -->
   </tbody>
+
 </template>
 
 <script>
