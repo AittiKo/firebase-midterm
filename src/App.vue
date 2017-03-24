4f5b3f8c438742d74d2b19f65f4d9806f049185c
thanawatumarsa <thanawatumarsa@gmail.com>
2017-03-24 21:16:37 +0700

    implement component

--- src/App.vue
+++ src/App.vue
@@ -3,65 +3,8 @@
     <div class="page-header">
       <h1>Vue.js 2 & Firebase Sample Application</h1>
     </div>
-
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3>Add book</h3>
-      </div>
-      <div class="panel-body">
-        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
-          <div class="form-group">
-            <label for="bookTitle">Title:</label>
-            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
-          </div>
-          <div class="form-group">
-            <label for="bookAuthor">Author:</label>
-            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
-          </div>
-          <div class="form-group">
-            <label for="bookUrl">URL:</label>
-            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
-          </div>
-          <input type="submit" class="btn btn-primary" value="Add Book">
-        </form>
-      </div>
-    </div>
-
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3>Books Lists</h3>
-      </div>
-      <div class="panel-body">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>
-                Title
-              </th>
-              <th>
-                Author
-              </th>
-              <th>
-                Delete
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="book in books">
-              <td>
-                <a v-bind:href="book.url">{{book.title}}</a>
-              </td>
-              <td>
-                {{book.author}}
-              </td>
-              <td>
-                <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <add-book :addbook = "addBook" :newbook="newBook"></add-book>
+    <book-lists :books="books" :removebook="removeBook"></book-lists>
   </div>
 </template>
 
@@ -69,6 +12,9 @@
 
 import Firebase from 'firebase'
 import toastr from 'toastr'
+
+import BookLists from './components/BookLists'
+import AddBook from './components/AddBook'
 
 let config = {
   apiKey: 'AIzaSyDwuD02hYiZTf2uVv0exIX6QoWe5vDvk60',
@@ -97,9 +43,12 @@ export default {
       }
     }
   },
+  components: {
+    BookLists,
+    AddBook
+  },
   methods: {
     addBook: function () {
-      console.log(this.newBook.title)
       booksRef.push(this.newBook)
       toastr.success('Add new Book')
       this.newBook.title = ''
---
+++ src/components/AddBook.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3>Add book</h3>
+    </div>
+    <div class="panel-body">
+      <form id="form" class="form-inline" v-on:submit.prevent="addbook">
+        <div class="form-group">
+          <label for="bookTitle">Title:</label>
+          <input type="text" id="bookTitle" class="form-control" v-model="newbook.title">
+        </div>
+        <div class="form-group">
+          <label for="bookAuthor">Author:</label>
+          <input type="text" id="bookAuthor" class="form-control" v-model="newbook.author">
+        </div>
+        <div class="form-group">
+          <label for="bookUrl">URL:</label>
+          <input type="text" id="bookUrl" class="form-control" v-model="newbook.url">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Add Book">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['addbook', 'newbook']
+}
+</script>
+
+<style lang="css">
+</style>
